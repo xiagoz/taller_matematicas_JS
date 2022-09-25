87e52f043b748bfcc5ea8fd646b1774051d9f0c2
xiagoz <christ2194.gn@gmail.com>
2022-09-25 07:41:08 -0500

    modulo 1

--- src/js/math.js
+++ src/js/math.js
@@ -47,3 +47,31 @@ console.log({
     areaTriangulo
 });
 console.groupEnd('Triangulo');
+//Formulas del circulo
+console.group('Circulo');
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo **2) * PI;
+
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    circunferencia,
+    areaCirculo
+})
+
+function calcCirculo (radio){
+    const diametro = radio * 2;
+    const radioAlCuadrado = Math.pow(radio, 2);
+
+    return {
+        circunferencia: diametro * Math.PI.toFixed(2),
+        area: radioAlCuadrado * Math.PI.toFixed(2),
+    }
+}
+console.log(calcCirculo(radioCirculo));
+console.groupEnd('circulo');
